610a0be30cd09964a0498a4aa1983aaaeed33261
StoRm619 <matthewsabebe23@gmail.com>
2021-12-02 01:27:53 -0800

    added right side header

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,10 +2,11 @@ import Image from 'next/image';
 import Logo from "../assets/ODP.png"
 import { BellIcon, ChatIcon, ChevronDownIcon, HomeIcon, UserGroupIcon, ViewGridIcon } from "@heroicons/react/solid";
 import { FlagIcon, PlayIcon, SearchIcon, ShoppingCartIcon } from "@heroicons/react/outline";
-
+import HeaderIcon from './HeaderIcon';
 function Header() {
     return (
-        <div>
+        <div className="sticky top-0 z-50 bg-white flex items-center p-2
+        lg:px-5 shadow-md">
             <div className="flex items-center">
                 <Image src={Logo}
                     width={40}
@@ -15,12 +16,31 @@ function Header() {
                 <div className="flex ml-2 items-center rounded-full bg-gray-100 p-2">
                     <SearchIcon className='h-6 text-gray-600' />
                     <input
-                        className="flex ml-2 items-center bg-transparent outline-none 
-                        placeholder-gray-500"
+                        className="hidden md:inline-flex ml-2 items-center bg-transparent outline-none 
+                        placeholder-gray-500 flex-shrink"
                         type="text"
                         placeholder="searchODP" />
                 </div>
             </div>
+
+            <div className="flex justify-center flex-grow">
+                <div className="flex space-x-6 md:space-x-2">
+                    <HeaderIcon active Icon={HomeIcon} />
+                    <HeaderIcon Icon={FlagIcon} />
+                    <HeaderIcon Icon={PlayIcon} />
+                    <HeaderIcon Icon={ShoppingCartIcon} />
+                    <HeaderIcon Icon={UserGroupIcon} />
+
+                </div>
+            </div>
+            <div className="flex items-center sm:space-x-2 justify-end">
+                <p className="whitespace-nowrap font-semibold pr-3">Name and profile pic</p>
+                <ViewGridIcon className="icon" />
+                <BellIcon className="icon" />
+                <ChatIcon className="icon" />
+                <ChevronDownIcon className="icon" />
+            </div>
+
         </div>
     )
 }
