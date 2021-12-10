4d3a2ad5cd007ec63f5fa263ebe2b29e09d2da51
StoRm619 <matthewsabebe23@gmail.com>
2021-12-10 02:36:38 -0800

    google auth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,9 +4,16 @@ import Providers from "next-auth/providers";
 export default NextAuth({
     // Configure one or more authentication providers
     providers: [
-        Providers.Facebook({
-            clientId: process.env.FACEBOOK_CLIENT_ID,
-            clientSecret: process.env.FACEBOOK_CLIENT_SECRET
+        Providers.Google({
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+            authorization: {
+                params: {
+                    prompt: "consent",
+                    access_type: "offline",
+                    response_type: "code"
+                }
+            }
         })
     ]
 })
