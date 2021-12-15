817e0754192a598d8129a8f46f4001960a4655bb
StoRm619 <matthewsabebe23@gmail.com>
2021-12-14 22:11:16 -0800

    added google

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -6,6 +6,10 @@ const options = {
             clientId: process.env.GITHUB_ID,
             clientSecret: process.env.GITHUB_SECRET
         }),
+        Providers.Google({
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET
+        })
     ],
 }
 export default (req, res) => NextAuth(req, res, options)
